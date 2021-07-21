0c3911e5664a8ce05a8772609cbd633625331bb8
WherbethDiniz <wherbethdinizs@gmail.com>
2021-07-21 17:27:59 -0300

    update project

--- src/main/java/br/com/springbootcrud/domain/CdProjektUser.java
+++ src/main/java/br/com/springbootcrud/domain/CdProjektUser.java
@@ -27,7 +27,7 @@ public class CdProjektUser implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotEmpty(message = "The anime name cannot be empty")
+    @NotEmpty(message = "The user's name cannot be empty")
     private String name;
 
     private String username;
